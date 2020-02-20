a24924f5f4a1af186038938a10b3667985167b54
Maharaj Syed <maharajsyed@yahoo.com>
2020-02-20 23:05:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,13 @@ def introduction (name)
 end
 
 def introduction_with_language (name, language)
-  puts "Hi, my name is #{name}"
+  puts "Hi, my name is #{name} and I am learning to program #{language}."
+end
+
+def introduction_with_language_optional (name, language ="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program #{language}."
+end
+
 
 
 
